0f6b0eb18d919368a905783abd8a658402553278
Grey <sergejyurskyj@yahoo.com>
2020-09-01 14:36:03 -0700

    Committed 04+A+formal+introduction+to+integers

---
+++ 000_for_templates/03 A little fast pace cpp/04+A+formal+introduction+to+integers/main.cpp
@@ -0,0 +1,23 @@
+////
+////  main.cpp
+////  get_started
+////
+////  Created by syurskyi on 01/09/20.
+//
+//
+//? i..
+//? c..
+//
+//u.. s..
+//
+//i.. main
+//    //1 byte is of 8 bits
+//
+//    p..("Size of this data type is %ld bits\n", s_o_(uint16_t) * 8)
+//
+//    i.. fun _ 0b00010110
+//    p..("Fun value is @d\n" ?
+//
+//
+//    r_ 0
+//
