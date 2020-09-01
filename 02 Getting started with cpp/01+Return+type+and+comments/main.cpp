72a13b7d1d9c2e5a8d86296fe70c0fbc5b520773
Grey <sergejyurskyj@yahoo.com>
2020-09-01 11:37:40 -0700

    Committed 06+Get+the+color+and+assignment

---
+++ 02 Getting started with cpp/01+Return+type+and+comments/main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by lco on 18/04/20.
+//  Copyright © 2020 lco. All rights reserved.
+//
+
+//#include <iostream>
+//
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    std::cout << "Hello, World!\n";
+//    return 0;
+//}
+
+#include <cstdio>
+using namespace std;
+
+int main(){
+    puts("Click on Button");
+    puts("create a new player");
+    puts("add life to player");
+    return 0; //return an exit with 0 code
+}
+
+/*
+ here I
+ can write multi line
+ comment
+ */
+
+// this is for your ref.
+void nothing(){
+    puts("Hello World!");
+}
+
+char iamachar(){
+    return 'c';
+}
