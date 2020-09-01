6e93f06852dcaa688e76189e34a88f26682ac4fd
Grey <sergejyurskyj@yahoo.com>
2020-09-01 14:31:32 -0700

    Committed 02+Reference+is+the+actual+tough+thing+in+cpp

---
+++ 000_for_templates/03 A little fast pace cpp/02+Reference+is+the+actual+tough+thing+in+cpp/main.cpp
@@ -0,0 +1,26 @@
+////
+////  main.cpp
+////  get_started
+////
+////  Created by syurskyi on 01/09/20.
+//
+//
+//? i..
+//u.. s..
+//
+//i.. main
+//
+//    i.. score _ 200
+//    i.. #myp _ #>
+//
+//    p..("Value of score is @d\n" ?
+//    p..("Value of pointer is @p\n" ?
+//
+//    i.. #another_score _ s..
+//    ? _ 800
+//
+//    p..("Value of score is @d\n" ?
+//    p..("Value of pointer is @p\n" ?
+//
+//    r_ 0
+//
