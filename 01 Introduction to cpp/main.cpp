416efeb918749cbcf1a24cb2783d1e39363e36f2
Grey <sergejyurskyj@yahoo.com>
2020-09-01 11:32:15 -0700

    Committed 01 Introduction to cpp

---
+++ 01 Introduction to cpp/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by syurskyi on 01/09/20.
+
+
+//#include <iostream>
+//
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    std::cout << "Hello, World!\n";
+//    return 0;
+//}
+
+#include <cstdio>
+using namespace std;
+
+int main(){
+    puts("Click on Button");
+    puts("create a new player");
+    puts("add life to player");
+    return 0;
+}
